3d23272655167fc31fb0ca686177acf1e85d2837
Brady Wied <brady@bswtechconsulting.com>
2017-11-18 10:29:27 -0700

    Lay out basics

--- src/main/groovy/com/brady/WinSSOFriendlyWagon.groovy
+++ src/main/groovy/com/brady/WinSSOFriendlyWagon.groovy
@@ -1,28 +1,41 @@
 package com.brady
 
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.WinHttpClients
 import org.apache.maven.wagon.*
 import org.apache.maven.wagon.authentication.AuthenticationException
 import org.apache.maven.wagon.authorization.AuthorizationException
 
 class WinSSOFriendlyWagon extends StreamWagon {
+    // this class is instantiated for each repo
+    private CloseableHttpClient httpClient
+    private boolean closed
+
     @Override
     void fillInputData(
             InputData inputData) throws TransferFailedException, ResourceDoesNotExistException, AuthorizationException {
-
+        println "in theory we would do something in inputData here for - ${inputData.resource}"
     }
 
     @Override
     void fillOutputData(OutputData outputData) throws TransferFailedException {
-
+        throw new Exception('This wagon provider should not use this!')
     }
 
     @Override
     protected void openConnectionInternal() throws ConnectionException, AuthenticationException {
-        println "our job would be to 'open a connection' to repo ${repository}"
+        assert !httpClient: "Didn't expect to already have a client for ${repository}!"
+        def builder = WinHttpClients.custom()
+        // some proxies won't accept outbound traffic without a user agent
+        builder.userAgent = 'AHC'
+        httpClient = builder.build()
     }
 
     @Override
     void closeConnection() throws ConnectionException {
-
+        if (!closed) {
+            httpClient.close()
+            closed = true
+        }
     }
 }
