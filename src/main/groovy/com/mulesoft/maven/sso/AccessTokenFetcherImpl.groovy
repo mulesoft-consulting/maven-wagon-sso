68b8ab55ae118f809991cec73a2835843d3c1fb3
Brady Wied <brady@bswtechconsulting.com>
2017-11-20 18:44:27 -0600

    Replace htmlunit with simple HTML parser

--- src/main/groovy/com/mulesoft/maven/sso/AccessTokenFetcherImpl.groovy
+++ src/main/groovy/com/mulesoft/maven/sso/AccessTokenFetcherImpl.groovy
@@ -1,74 +1,95 @@
 package com.mulesoft.maven.sso
 
-import com.gargoylesoftware.htmlunit.HttpMethod
-import com.gargoylesoftware.htmlunit.Page
-import com.gargoylesoftware.htmlunit.WebClient
-import com.gargoylesoftware.htmlunit.WebRequest
-import com.gargoylesoftware.htmlunit.html.HtmlInput
-import com.gargoylesoftware.htmlunit.html.HtmlPage
 import groovy.json.JsonSlurper
 import groovy.util.logging.Slf4j
 import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.client.WinHttpClients
 import org.apache.http.message.BasicNameValuePair
 import org.apache.maven.wagon.proxy.ProxyInfo
+import org.jsoup.Jsoup
 
 @Slf4j
 class AccessTokenFetcherImpl implements AccessTokenFetcher {
-    private final WebClient client
     private final String anypointProfileUrl
     private final String samlIdpUrl
+    private final HttpClientBuilder clientBuilder
 
     AccessTokenFetcherImpl(ProxyInfo proxyInfo,
                            String anypointProfileUrl,
                            String samlIdpUrl) {
         this.samlIdpUrl = samlIdpUrl
         this.anypointProfileUrl = anypointProfileUrl
-        client = new WindowsFriendlyWebClient(proxyInfo)
-        def options = client.options
-        options.javaScriptEnabled = false
-        options.cssEnabled = false
+        clientBuilder = WinHttpClients.custom()
+        if (proxyInfo) {
+            clientBuilder.routePlanner = new WagonProxyInfoRoutePlanner(proxyInfo)
+        }
     }
 
     String getAccessToken() {
         log.info "Triggering initial SAML flow with {}",
                  samlIdpUrl
-        def responsePage = client.getPage(samlIdpUrl)
-        handleSamlResponse(responsePage)
-        // now we've done the equivalent of taking our browser all the way to the anypoint page, should have
-        // enough cookies to access the profile page, where we can grab a token
-        log.info "SAML Flow complete, now fetching access token from {}",
-                 anypointProfileUrl
-        def jsonProfile = client.getPage(anypointProfileUrl)
-        def map = new JsonSlurper().parse(jsonProfile.webResponse.contentAsStream)
-        def token = map['access_token']
-        assert token: "Unable to find access token in ${map}"
-        log.info "Access token fetched, successfully authenticated as '{}'.",
-                 map['username']
-        token
+        CloseableHttpClient client = null
+        CloseableHttpResponse response = null
+        try {
+            client = clientBuilder.build()
+            def get = new HttpGet(samlIdpUrl)
+            response = client.execute(get)
+            handleSamlResponse(response,
+                               client,
+                               samlIdpUrl)
+            response.close()
+            response = null
+            // now we've done the equivalent of taking our browser all the way to the anypoint page, should have
+            // enough cookies to access the profile page, where we can grab a token
+            log.info "SAML Flow complete, now fetching access token from {}",
+                     anypointProfileUrl
+            get = new HttpGet(anypointProfileUrl)
+            response = client.execute(get)
+            def map = new JsonSlurper().parse(response.entity.content)
+            def token = map['access_token']
+            assert token: "Unable to find access token in ${map}"
+            log.info "Access token fetched, successfully authenticated as '{}'.",
+                     map['username']
+            token
+        }
+        finally {
+            if (response) {
+                response.close()
+            }
+            client.close()
+        }
     }
 
-    private void handleSamlResponse(Page responsePage) {
-        assert responsePage.htmlPage: "Expected ${responsePage.url} to be a web page!"
-        assert responsePage instanceof HtmlPage
-        def samlForm = responsePage.forms.find { form ->
-            form.getInputsByName('SAMLResponse').any()
+    private static void handleSamlResponse(CloseableHttpResponse response,
+                                           CloseableHttpClient client,
+                                           String url) {
+        def parsedDocument = Jsoup.parse(response.entity.content,
+                                         'utf-8',
+                                         url)
+        def samlForm = parsedDocument.getElementsByTag('form')
+                .find { form ->
+            form.getElementsByAttributeValue('name', 'SAMLResponse').any()
         }
-        assert samlForm: "Expected a SAML form in response! ${responsePage.webResponse.contentAsString}"
-        def request = new WebRequest(samlForm.actionAttribute.toURL())
-        request.httpMethod = HttpMethod.POST
-        def params = samlForm.childElements
-                .findAll { element -> element instanceof HtmlInput }
-                .collect { HtmlInput input ->
-            new BasicNameValuePair(input.nameAttribute,
-                                   input.valueAttribute)
+        assert samlForm: "Expected a SAML form in response! ${parsedDocument}"
+        def parameters = samlForm.getElementsByTag('input').collect { input ->
+            new BasicNameValuePair(input.attr('name'),
+                                   input.attr('value'))
         }
-        def entity = new UrlEncodedFormEntity(params)
-        request.requestBody = entity.content.text
-        def header = entity.contentType
-        request.setAdditionalHeader(header.name,
-                                    header.value)
+        def post = new HttpPost(samlForm.attr('action'))
         log.info 'Posting SAML assertions back to {}',
-                 request.url
-        client.getPage(request)
+                 post.getURI()
+        post.entity = new UrlEncodedFormEntity(parameters)
+        response = client.execute(post)
+        try {
+
+        }
+        finally {
+            response.close()
+        }
     }
 }
